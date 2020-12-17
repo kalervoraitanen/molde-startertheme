9296496c03a2c7b18430d7ec0989b968d2daa9b1
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2020-12-17 09:37:57 +0200

    Added comment template

--- single.php
+++ single.php
@@ -59,6 +59,13 @@
 
   </article>
   
+	  <?php
+	  // If comments are open or there is at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) {
+			comments_template();
+		}
+		?>
+  
 	<?php endwhile; ?>
 
 	<?php /* If no posts found show error messages */ ?>

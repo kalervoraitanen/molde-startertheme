2e13b6421db77cb0a1da7709c60abb06c2b41322
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-02-11 09:50:36 +0200

    Added comment-reply script

--- functions.php
+++ functions.php
@@ -193,6 +193,10 @@ function molde_scripts_and_styles() {
     //wp_enqueue_script( 'modernizr' );
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'molde-js' );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+      wp_enqueue_script( 'comment-reply' );
+    }      
   }
 }
 

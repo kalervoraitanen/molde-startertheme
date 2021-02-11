25cc8622008d52f1a7f9b8b7562792f5f103ffe2
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-02-11 10:53:05 +0200

    Moved article markup to post format files

--- single.php
+++ single.php
@@ -5,41 +5,16 @@
 	<?php /* Loop through post data */ ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
+		<?php
+			 /*
+			 * This function will bring in the needed template file depending on what the post
+			 * format is. The different post formats are located in the post-formats folder.
+				*/
+			get_template_part( 'post-formats/format', get_post_format() );
+		?>
 
-	  <header class="entry-header">
-
-	    <h1 class="entry-title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
-
-	    <p class="entry-date">
-	      <span class="screen-reader-text"><?php echo __( 'Posted', 'molde-theme' ) ?> </span>
-	      <time class="updated entry-time" datetime="<?php echo get_the_time('Y-m-d h:i') ?>" itemprop="datePublished"><?php echo get_the_date('d/m/Y H:i') ?></time>
-	    </p>
-
-	  </header>
-
-    <section class="entry-content cf" itemprop="articleBody">
-
-    	<?php /* If post has feature image show it */ ?>
-			<?php if ( has_post_thumbnail() ) { ?>
-				<span itemprop="image" class="feat-image"><?php the_post_thumbnail(); ?></span>
-			<?php } ?>
-
-			<?php /* Show post content */ ?>
-      <?php
-        the_content();
-			?>
-
-    </section>
-
-		<footer class="entry-footer">
-
-			<?php molde_categories_tags(); ?>
-
-		</footer>
-
-  </article>
-  
+		<?php wp_link_pages(); ?>
+	  
 	  <?php
 	  // If comments are open or there is at least one comment, load up the comment template.
 		if ( comments_open() || get_comments_number() ) {

e949b60bfba176d80feec31a7231634d5c2a27ce
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-03-01 10:59:24 +0200

    Accessibility fixes for search form

--- searchform.php
+++ searchform.php
@@ -1,8 +1,10 @@
 <form role="search" method="get" class="searchform" itemscope itemtype="https://schema.org/SearchAction" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   
-  <label for="s" class="screen-reader-text"><?php _e('Search for:','molde-theme'); ?></label>
+  <label><span class="screen-reader-text"><?php _e('Search for:','molde-theme'); ?></span>
   
-  <input type="search" id="s" name="s" itemprop="query" placeholder="<?php _e('Search for...','molde-theme'); ?>" value="" />
+  <input type="search" name="s" itemprop="query" placeholder="<?php _e('Search for...','molde-theme'); ?>" value="" />
+
+  </label>
 
   <button type="submit" class="searchsubmit" ><span class="screen-reader-text"><?php _e('Search','molde-theme'); ?></span><span class="search-glass">&#9906;</span></button>
 

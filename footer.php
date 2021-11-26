aafbfa4219c0a8d6d2f60aa7da7fae4b89f07afc
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-11-26 19:36:19 +0200

    Removed copyright date.

--- footer.php
+++ footer.php
@@ -1,8 +1,5 @@
 		<footer role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
-			<?php /* Show copyright info by default. You can remove this if you want. */ ?>
-			&copy; Copyright <?php esc_html_e(date("Y")); ?>
-
 		</footer>
 
 		<?php wp_footer(); ?>

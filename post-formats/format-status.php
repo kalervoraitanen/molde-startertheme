f38c10ccc635d933e76e3f179f1361985c26ec5c
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-11-29 13:35:29 +0200

    Fixed timestamp to show user preferred date format.

--- post-formats/format-status.php
+++ post-formats/format-status.php
@@ -6,7 +6,7 @@
 
       <p class="entry-date">
         <span class="screen-reader-text"><?php echo esc_html__( 'Posted', 'molde-theme' ) ?> </span>
-        <time class="updated entry-time" datetime="<?php echo esc_attr(get_the_time('c')); ?>" itemprop="datePublished"><?php echo get_the_date(); esc_html_e( ' at ', 'molde-theme' ); echo get_the_date('H:i'); ?></time>
+        <time class="updated entry-time" datetime="<?php echo esc_attr(get_the_time('c')); ?>" itemprop="datePublished"><?php echo get_the_date(); esc_html_e( ' at ', 'molde-theme' ); echo get_the_time(); ?></time>
       </p>
 
     </header>

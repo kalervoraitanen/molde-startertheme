3a5fc7758c29da0241ca06e7212c4b133a32d467
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-11-30 19:18:58 +0200

    Added class for footer.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-		<footer role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+		<footer class="site-footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
 			<p>Proudly powered by <a href="https://www.wordpress.org" target="_blank" rel="noopener">WordPress</a>.</p>
 			

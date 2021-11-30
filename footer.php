a69e2c3e0435eeb722857537189199746f697084
kalervoraitanen <75725276+kalervoraitanen@users.noreply.github.com>
2021-11-30 19:16:18 +0200

    Added wordpress.org credit link.

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 		<footer role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
+			<p>Proudly powered by <a href="https://www.wordpress.org" target="_blank" rel="noopener">WordPress</a>.</p>
+			
 		</footer>
 
 		<?php wp_footer(); ?>
